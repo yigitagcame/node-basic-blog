cf564175e5f0c54943f29f16904ad6443d0762b4
yiitinki <yiitinki@gmail.com>
2019-08-11 16:15:38 +0300

    config sample file added

---
+++ config.sample.js
@@ -0,0 +1,13 @@
+// Change the file name to config.js
+const config = {}
+
+config.db = {
+    url : '',
+    port : '',
+    name : '',
+    user : '',
+    pass : ''
+}
+
+
+module.exports = config;
